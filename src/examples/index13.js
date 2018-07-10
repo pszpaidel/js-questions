7cdb3c97b214d06390cb68928f4af9abd6357f95
pszpaidel <szpaidel.pawel@gmail.com>
2018-07-10 23:45:15 +0200

    update

---
+++ src/examples/index13.js
@@ -0,0 +1,15 @@
+function change(a, b, c) {
+	a = a * 10;
+	b.item = "changed";
+  	c = {item: "changed"};
+}
+
+var num = 10;
+var obj1 = {item: "unchanged"};
+var obj2 = {item: "unchanged"};
+
+change(num, obj1, obj2);
+
+console.log(num);
+console.log(obj1.item);    
+console.log(obj2.item);
